392362e13b2310eca3e6721add4093e6e9923476
mrfrase3 <mrfrase3@gmail.com>
2022-09-01 10:53:54 +0800

    increase req body limit

--- src/index.js
+++ src/index.js
@@ -18,8 +18,8 @@ server.listen(process.env.PORT || 3030);
 app.options('*', cors());
 app.use(cors());
 app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 io.on('connection', async (socket) => {
   socket.on('info.server', (done) => info.server(socket, done));
